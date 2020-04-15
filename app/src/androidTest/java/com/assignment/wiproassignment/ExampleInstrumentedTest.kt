e11b4b40c4e1b454069e8e9c03d06225b7088eff
AparnaS-creator <ascafe87@gmail.com>
2020-04-15 09:06:07 +0530

    Unit test case

--- app/src/androidTest/java/com/assignment/wiproassignment/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/assignment/wiproassignment/ExampleInstrumentedTest.kt
@@ -1,5 +1,8 @@
 package com.assignment.wiproassignment
 
+import android.app.Activity
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,18 +10,36 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@Suppress("DEPRECATION")
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    lateinit var instrumentationContext: Context
+
+    @Before
+    fun setup() {
+        instrumentationContext = InstrumentationRegistry.getInstrumentation().context
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.assignment.wiproassignment", appContext.packageName)
+    }
+
+    @Test
+    fun checkNetwork() {
+        val connectivityManager = instrumentationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        assertEquals(true,networkInfo.isConnected)
+
     }
 }
